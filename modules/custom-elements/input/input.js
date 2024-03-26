ab451c61ae5c692d9325b18eb9673a3bb382110d
vyachnd <vyacheslav.nodata@yandex.ru>
2024-03-26 15:36:31 +0300

    refactor: add default cls for custom-input

--- modules/custom-elements/input/input.js
+++ modules/custom-elements/input/input.js
@@ -12,7 +12,7 @@ class CustomInput {
       rounded: false,
       leftElement: null,
       rightElement: null,
-      cls: [],
+      cls: ['custom-input'],
       ...options,
     };
     this.parent = null;
@@ -104,7 +104,7 @@ class CustomInput {
     if (!input) return null;
 
     if (input.classList.length > 0) input.className = '';
-    input.classList.add('custom-input', ...this.options.cls);
+    input.classList.add(...this.options.cls);
 
     if (this.options.id && inputField.id !== this.options.id) inputField.id = this.options.id;
     if (!this.options.id) delete inputField.id;
