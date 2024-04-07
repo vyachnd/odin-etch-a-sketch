6334ecda76786fd0dc0a2931ad36eb5f15ed95bb
vyachnd <vyacheslav.nodata@yandex.ru>
2024-04-07 23:09:47 +0300

    feat: add script.js for developing

--- script.js
+++ script.js
@@ -1,1 +1,102 @@
-console.log('Etch-a-sketch');
+/*
+  TODO:
+    - [x] Brush tool
+    - [x] Fill tool
+    - [x] Eraser tool
+    - [ ] Shading tool
+    - [ ] Lighting tool
+    - [x] Color selector tool
+    - [ ] Background color selector tool
+    - [ ] Rainbow color tool
+    - [ ] Grid size change tool
+
+    - [ ] History redo/undo
+    - [ ] Zoom in/reset/out
+    - [ ] Move camera on board center
+    - [ ] Clear board
+    - [ ] Save board as image
+*/
+
+// Helper functions
+import unzoom from './libraries/unzoom.js';
+import fontsMSPreload from './modules/custom-elements/materialSymbols.js';
+
+// Elements
+import Board from './modules/board/board.js';
+import Camera from './modules/camera/camera.js';
+import CustomHeader from './modules/custom-elements/header/header.js';
+import CustomIcon from './modules/custom-elements/icon/icon.js';
+import Plate from './modules/plate.js';
+
+// Tools
+import toolBrushInit from './modules/tools/brush/init.js';
+import toolColorInit from './modules/tools/color/init.js';
+import toolDragInit from './modules/tools/drag/init.js';
+import toolEraserInit from './modules/tools/eraser/init.js';
+import toolFillInit from './modules/tools/fill/init.js';
+import toolGridInit from './modules/tools/grid/init.js';
+import toolThumbInit from './modules/tools/thumb/init.js';
+
+function initApp() {
+  // App
+  const appElement = document.createElement('div');
+  appElement.id = 'app';
+  document.body.prepend(appElement);
+
+  // Create camera and board
+  const camera = new Camera();
+  const board = new Board({ entity: { grid: { rows: 16, cols: 16 } } });
+
+  camera.render(appElement);
+  camera.addEntity(board);
+
+  board.move({
+    x: -board.target.offsetWidth / 2,
+    y: -board.target.offsetHeight / 2,
+  });
+
+  camera.move(camera.fieldCenter);
+
+  // Tools
+  const { toolColor, toolColorBtn } = new toolColorInit(board);
+
+  const { toolDrag, toolDragBtn } = new toolDragInit(camera);
+  const { toolThumb, toolThumbBtn } = new toolThumbInit(board);
+  const { toolGrid, toolGridBtn } = new toolGridInit(board);
+  const { toolBrush, toolBrushBtn } = new toolBrushInit(board, toolColor);
+  const { toolFill, toolFillBtn } = new toolFillInit(board, toolColor);
+  const { toolEraser, toolEraserBtn } = new toolEraserInit(board);
+
+  toolDrag.disable();
+  toolThumb.enable();
+  toolGrid.enable();
+  toolBrush.enable();
+  toolFill.enable();
+  toolEraser.enable();
+
+  // Header
+  const headerElement = new CustomHeader({
+    heading: 'Etch-A-Sketch'.toUpperCase(),
+    subheading: 'by ${vyachnd}',
+    url: 'https://github.com/vyachnd',
+    icon: new CustomIcon({ type: 'rounded', fill: true, opsz: 40, icon: 'palette' }),
+    cls: ['top', 'left'],
+  });
+  headerElement.render(appElement);
+
+  // Toolbar
+  const toolbarElement = new Plate({
+    cls: ['top', 'center'],
+  });
+  toolbarElement.render(appElement);
+  toolbarElement.addElement(toolDragBtn, 'toolDragBtn');
+  toolbarElement.addElement(toolThumbBtn, 'toolThumbBtn');
+  toolbarElement.addElement(toolGridBtn, 'toolGridBtn');
+  toolbarElement.addElement(toolBrushBtn, 'toolBrushBtn');
+  toolbarElement.addElement(toolFillBtn, 'toolFillBtn');
+  toolbarElement.addElement(toolEraserBtn, 'toolEraserBtn');
+  toolbarElement.addElement(toolColorBtn, 'toolColorBtn');
+}
+
+unzoom(window);
+fontsMSPreload(initApp);
