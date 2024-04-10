e99c4a6f05e5c265dd97d5a025ff86493195a176
vyachnd <vyacheslav.nodata@yandex.ru>
2024-04-10 18:18:24 +0300

    fix: brush every time if mouse move

--- modules/tools/brush/brush-logic.js
+++ modules/tools/brush/brush-logic.js
@@ -1,8 +1,11 @@
+import { objectsEqual } from '../../../libraries/helpers.js';
 
 
 class BrushLogic {
   constructor(entity) {
     this._entity = entity;
+
+    this.prevPosition = null;
 
     this.board.emitter.on('onMouseDown', this.handleMouseDown.bind(this));
     this.board.emitter.on('onMouseMove', this.handleMouseMove.bind(this));
@@ -29,12 +32,23 @@ class BrushLogic {
     if (!this.isEnabled) return;
     if (data.button !== 0) return;
 
+    this.prevPosition = data.position;
+
     this._entity.onBrush(data.position, event);
   }
   handleMouseMove(data, event) {
     if (!this.isEnabled) return;
     if (!data.down) return;
     if (data.button !== 0) return;
+
+    const cells = {
+      current: this.board.posToCell(data.position),
+      prev: this.board.posToCell(this.prevPosition),
+    };
+
+    if (objectsEqual(cells.current, cells.prev)) return;
+
+    this.prevPosition = data.position;
 
     this._entity.onBrush(data.position, event);
   }
